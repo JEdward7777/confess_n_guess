545b5e32bedf3fa6192033e64843954397d7c074
Joshua Lansford <HootOwl7777@gmail.com>
2024-02-24 13:47:48 -0500

    Serving react through express server.

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+var httpPath = path.join( __dirname, "fibbage_knockoff_client/dist/" );
+
+app.use(express.static(httpPath));
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
